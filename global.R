5284b8e9dd3f475346f6da8fd5d9e6e72d997321
Steeefan <sh@steeefan.de>
2017-02-07 08:30:56 -0500

    wday handling from global.R to makeTSDateFields.py

--- global.R
+++ global.R
@@ -3,10 +3,6 @@ library(shinydashboard)
 library(dplyr)
 library(lubridate)
 
-wdayDE = function(wday) {
-  switch(wday, 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
-}
-
 songs = readRDS('data/clean/swr3-songs-2016-v3.rds')
 tz = 'Europe/Berlin'
 dtFormat = '%Y-%m-%d %H:%M:%S'
@@ -15,11 +11,6 @@ songs = mutate(
   ts = as.POSIXct(strptime(ts, tz=tz, format=dtFormat)),
   title = as.character(title),
   artist = as.character(title),
-  weekdayLbl = wdayDE(weekday)  # Week starts on Mon in DE!
+  wdayLbl = as.character(wdayLbl)
 )
 
-songs %>% group_by(weekday) %>% summarise(n())
-
-# for (i in 1:nrow(songs)) {
-#   print(paste(i, songs[i, 'weekday'], wdayDE(songs[i, 'weekday'])))
-# }
--- makeTSDateFields.py
+++ makeTSDateFields.py
@@ -63,14 +63,15 @@ songs['day'] = songs.apply(lambda row: row['ts'].day, axis=1)
 songs['month'] = songs.apply(lambda row: row['ts'].month, axis=1)
 songs['year'] = songs.apply(lambda row: row['ts'].year, axis=1)
 
-songs['weekday'] = songs.apply(lambda row: row['ts'].isoweekday(), axis=1)
+songs['wday'] = songs.apply(lambda row: row['ts'].isoweekday(), axis=1)
+songs['wdayLbl'] = songs.apply(lambda row: datetime.datetime.strftime(row['ts'], '%a'), axis=1)
 songs['week'] = songs.apply(lambda row: row['ts'].week, axis=1)
 songs['quarter'] = songs.apply(lambda row: row['ts'].quarter, axis=1)
 
 songs['hour'] = songs.apply(lambda row: row['ts'].hour, axis=1)
 songs['minute'] = songs.apply(lambda row: row['ts'].minute, axis=1)
 
-songs['rushHour'] = songs.apply(lambda row: max([x if row['hour'] >= rushHour[x]['from'] and row['hour'] <= rushHour[x]['to'] and row['weekday'] < 6 else '' for x in rushHour]), axis=1)
+songs['rushHour'] = songs.apply(lambda row: max([x if row['hour'] >= rushHour[x]['from'] and row['hour'] <= rushHour[x]['to'] and row['wday'] < 6 else '' for x in rushHour]), axis=1)
 songs['season'] = songs.apply(lambda row: max([x if row['ts'].date() >= season[x]['from'] and row['ts'].date() <= season[x]['to'] else '' for x in season]), axis=1)
 
 songs.to_csv(wdirF + 'swr3-songs-2016-v3.csv', index=False, quoting=csv.QUOTE_ALL)
